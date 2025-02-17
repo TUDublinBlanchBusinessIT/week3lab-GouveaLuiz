2a70d1d1cf775e3f70493d1c3eb423782b5fb6ee
GouveaLuiz <b00165757@mytudublin.ie>
2025-02-17 17:43:11 +0000

    Add files via upload

--- divisors.py
+++ divisors.py
@@ -1,25 +1,17 @@
-#Add your name here
-#Add the date here
-#Add the filename here
+#Luiz Gouvea
+#17/02/2025
+#divisors
 
-#Add a function below called divisors(num) which takes one argument of type integer
-#and returns a list of all the divisors(factors) of that that number -
-#A divisor or factor is a number which divides evenly leaving no remainder
+def divisors(n):
+    my_divisors_list = []
+    for i in range(1, n):
+        if n % i == 0:
+            my_divisors_list.append(i)
+    return my_divisors_list 
 
-#define the funciton header called divisors expecting one argument
+def main():
+    print(divisors(30))
 
-    #set up an empty list to hold your result
- 
-    
-    #loop i from 1 to the number you are checking (take care not to include the number itself)
-
-        #if the remainder when dividing the number by i is equal to zero then i is a divisor so...
-
-            #apend i to the list you set up
-
- 
-    #return the list
+main()
 
 
-#to test the divisors function uncomment the following line it should give [1,2,3,5,6,10,15]
-#print(divisors(30))
--- findFirstFourPNs.py
+++ findFirstFourPNs.py
@@ -1,6 +1,17 @@
-#Write a program here that finds the first four perfect numbers.
-#Put your name and date at the top of the file.
-#The pseudocode in the README.md file will help you write this program.
+#Luiz Gouvea
+#17/02/2025
+#findFirstFourPNs
 
+from perfectNumber import isPerfectNumber
 
+def find_first_four_perfect_numbers():
+    count = 0
+    num = 1
+    while count < 4:
+        if isPerfectNumber(num):
+            print(f"{num} is a perfect number")
+            count += 1
+        num += 1  
+
+find_first_four_perfect_numbers()
 
